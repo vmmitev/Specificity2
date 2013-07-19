51d3adc50f86e80e56c55fa3c1ef2bf06096c7be
William E. Kempf <wekempf@outlook.com>
2013-07-19 14:58:00 -0400

    Added Documentation solution.

---
+++ src/Specificity/NamespaceDoc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Specificity
+{
+    /// <summary>
+    /// The <see cref="N:Testing.Specificity"/> namespace contains classes that can be used in unit test
+    /// projects for specifying test assertions in a fluent and extensible manner, verifying conceptual contracts
+    /// of interfaces and simplifying the creation of objects necessary for a test to run but that isn't other wise
+    /// relevant.
+    /// </summary>
+    [CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}
